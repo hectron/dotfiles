d8cc5b63200b8ad196b6d00b3b075f6d25ee4241
Hector Rios <that.hector@gmail.com>
2021-04-20 12:09:37 -0500

    Format this

--- make_dotfiles.go
+++ make_dotfiles.go
@@ -72,46 +72,46 @@ func main() {
 	for _, dotfile := range dotfiles {
 		baseFilename := filepath.Base(dotfile)
 		newFilepath := filepath.Join(userHomeDir, baseFilename)
-    fileDescriptor, _ := os.Stat(newFilepath)
-    shouldCopy := true
+		fileDescriptor, _ := os.Stat(newFilepath)
+		shouldCopy := true
 
-    if fileDescriptor != nil {
-      question := fmt.Sprintf("%s exists. Override with repo version? (y/n)", newFilepath)
+		if fileDescriptor != nil {
+			question := fmt.Sprintf("%s exists. Override with repo version? (y/n)", newFilepath)
 
-      response, err := ui.Ask(question, &input.Options{
-        Required: true,
-        Loop:     true,
-        ValidateFunc: func(input string) error {
-          downcasedInput := strings.ToLower(input)
+			response, err := ui.Ask(question, &input.Options{
+				Required: true,
+				Loop:     true,
+				ValidateFunc: func(input string) error {
+					downcasedInput := strings.ToLower(input)
 
-          if downcasedInput != "y" && downcasedInput != "n" {
-            return fmt.Errorf("please enter y or n")
-          }
+					if downcasedInput != "y" && downcasedInput != "n" {
+						return fmt.Errorf("please enter y or n")
+					}
 
-          return nil
-        },
-      })
+					return nil
+				},
+			})
 
-      ExitIfError(err, 1)
+			ExitIfError(err, 1)
 
-      if response == "y" {
-        err := os.Remove(newFilepath)
+			if response == "y" {
+				err := os.Remove(newFilepath)
 
-        ExitIfError(err, 1)
+				ExitIfError(err, 1)
 
-        shouldCopy = true
-      } else {
-        shouldCopy = false
-        fmt.Sprintln("Not copying %s", baseFilename)
-      }
-    }
+				shouldCopy = true
+			} else {
+				shouldCopy = false
+				fmt.Sprintln("Not copying %s", baseFilename)
+			}
+		}
 
-    if shouldCopy {
-      err = os.Symlink(dotfile, newFilepath)
+		if shouldCopy {
+			err = os.Symlink(dotfile, newFilepath)
 
-      ExitIfError(err, 1)
+			ExitIfError(err, 1)
 
-      fmt.Printf("Symlinked %q to %q\n", dotfile, newFilepath)
-    }
+			fmt.Printf("Symlinked %q to %q\n", dotfile, newFilepath)
+		}
 	}
 }
