a133e7b8a4a3650b240c816d430034b7fbc0c19e
Hector Rios <that.hector@gmail.com>
2022-08-09 16:20:00 -0500

    feat: better cursorhold neovim

--- nvim/.config/nvim/lua/lsp/handlers.lua
+++ nvim/.config/nvim/lua/lsp/handlers.lua
@@ -46,11 +46,29 @@ function M.on_attach(client, bufnr)
         virtual_text = false, -- Turn off inline diagnostics
     })
     vim.api.nvim_create_autocmd({ "CursorHold" }, {
+        buffer = bufnr,
         callback = function()
-            if vim.lsp.buf.server_ready() then
-                vim.diagnostic.open_float()
+            local opts = {
+                focusable = false,
+                close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+                border = 'rounded',
+                source = 'always', -- show source in diagnostic popup window
+                prefix = ' '
+            }
+
+            if not vim.b.diagnostics_pos then
+                vim.b.diagnostics_pos = { nil, nil }
             end
-        end
+
+            local cursor_pos = vim.api.nvim_win_get_cursor(0)
+            if (cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2]) and
+                #vim.diagnostic.get() > 0
+            then
+                vim.diagnostic.open_float(nil, opts)
+            end
+
+            vim.b.diagnostics_pos = cursor_pos
+        end,
     })
 
     -- set up LSP signs
