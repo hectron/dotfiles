f30efd31a4943a6acb9c03aca1a790ae41767f38
Hector Rios <hrios@paypal.com>
2024-01-26 22:27:17 +0000

    fix: Properly lint README

--- nvim/.config/nvim/after/plugin/lspconfig.rc.lua
+++ nvim/.config/nvim/after/plugin/lspconfig.rc.lua
@@ -38,8 +38,8 @@ local lsp_servers = {
 
 mason_config.setup({})
 mason_lspconfig.setup({
-    automatic_installation = true,
-    ensure_installed = lsp_servers,
+  automatic_installation = true,
+  ensure_installed = lsp_servers,
 })
 
 -- Specify any custom settings for an LSP server here
@@ -53,28 +53,45 @@ local server_specific_opts = {
     opts.cmd = { 'gopls', '-remote=auto' }
   end,
   ['lua_ls'] = function(opts)
-    opts.settings = {
-      Lua = {
-          diagnostics = {
-            globals = {
-              'vim',
+    opts.on_init = function(client)
+      local path = client.workspace_folders[1].name
+      if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+        client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+          Lua = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using
+              -- (most likely LuaJIT in the case of Neovim)
+              version = 'LuaJIT',
             },
+            -- Make the server aware of Neovim runtime files
             workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
               checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME,
+              },
             },
-        }
-      }
-    }
-    end,
+            telemetry = {
+              enable = false,
+            },
+            hint = {
+              enable = true,
+            },
+          },
+        })
+
+        client.notify('workspace/didChangeConfiguration', { settings = client.config.settings })
+      end
+      return true
+    end
+  end,
 }
 
 local lsp_handlers = require("lsp/handlers")
 
 for _, server_name in pairs(lsp_servers) do
   local opts = {
-      on_attach = lsp_handlers.on_attach,
-      capabilities = lsp_handlers.capabilities,
+    on_attach = lsp_handlers.on_attach,
+    capabilities = lsp_handlers.capabilities,
   }
 
   if server_specific_opts[server_name] then
