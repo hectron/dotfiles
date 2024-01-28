2921213670d5d1a75ecfa13d9d55f9c05569365a
Hector Rios <that.hector@gmail.com>
2024-01-27 19:10:16 -0600

    feat: Add gitsigns to plugins, deprecate gitgutter

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -12,10 +12,48 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  "airblade/vim-gitgutter",
-  "tpope/vim-commentary", -- Easily comment
-  "tpope/vim-fugitive",   -- Git blame, diff, browse, etc
-  "tpope/vim-rhubarb",    -- View github commit using :GBrowse
+  -- "airblade/vim-gitgutter",
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+      on_attach = function(buffer)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, desc)
+          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+        end
+
+        -- stylua: ignore start
+        map("n", "]h", gs.next_hunk, "Next Hunk")
+        map("n", "[h", gs.prev_hunk, "Prev Hunk")
+        map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+        map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+        map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+        map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+        map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+        map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
+        map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+        map("n", "<leader>ghd", gs.diffthis, "Diff This")
+        map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+      end,
+    },
+  },
+  {
+    "tpope/vim-commentary", -- Easily comment
+    event = "VeryLazy",
+  },
+  "tpope/vim-fugitive", -- Git blame, diff, browse, etc
+  "tpope/vim-rhubarb",  -- View github commit using :GBrowse
   "tpope/vim-endwise",
   "benmills/vimux",
   "vim-test/vim-test", -- Run tests in conjuction with vimux
@@ -147,7 +185,7 @@ require("lazy").setup({
           end,
         }),
         sources = cmp.config.sources({
-          { name = "nvim_lsp" },
+          { name = "nvim_lsp" }, -- TODO why isn't this working?
           { name = "path" },
         }, {
           { name = "buffer" },
