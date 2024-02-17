02ccab0561682cd2d8f181cf81bef0cb29440ea3
Hector Rios <that.hector@gmail.com>
2024-02-17 16:30:08 -0600

    feat: Move treesitter inline

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -173,7 +173,56 @@ require("lazy").setup({
       "rcarriga/nvim-notify",
     },
   },
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "dockerfile",
+        "git_config",
+        "git_rebase",
+        "gitcommit",
+        "gitignore",
+        "go",
+        "hcl",
+        "javascript",
+        "json",
+        "lua",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "puppet",
+        "query",
+        "ruby",
+        "rust",
+        "yaml",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+      },                   -- this is available in the list of official parsers
+      ignore_install = {}, -- list of parsers to ignore installing
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = true,
+        use_languagetree = true,
+      },
+      incremental_selection = {
+        enable = true
+      },
+      textobjects = {
+        enable = true
+      },
+    },
+    config = function(_, opts)
+      local configs = require("nvim-treesitter.configs")
+      configs.setup(opts)
+    end,
+  },
   {
     "nvim-telescope/telescope.nvim",
     dependencies = {
