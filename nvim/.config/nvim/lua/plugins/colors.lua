d06e371d8097b733200706cb5e7269b372db0f1a
Hector Rios <that.hector@gmail.com>
2025-02-22 12:34:54 -0600

    feat: Use zenbones, blink.cmp, and catppuccin

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -6,13 +6,17 @@ return {
       flavour = "mocha",
       transparent_background = true,
     },
+    priority = 1000,
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme([[catppuccin]])
+    end
   },
   {
     "folke/tokyonight.nvim",
-    priority = 1000,
-    config = function(_, opts)
-      require("tokyonight").setup(opts)
-      vim.cmd.colorscheme("tokyonight")
-    end,
+    opts = {
+      dim_inactive = true,
+      transparent = true,
+    },
   },
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,1 +1,78 @@
-return {}
+local Util = require("utils")
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = Util.LazyFileEvents,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "nvim-treesitter/nvim-treesitter-context",
+      "RRethy/nvim-treesitter-endwise",
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        ft = { "markdown" },
+        opts = {},
+        dependencies = {
+          "nvim-tree/nvim-web-devicons",
+        },
+      },
+    },
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "diff",
+        "dockerfile",
+        "git_config",
+        "git_rebase",
+        "gitcommit",
+        "gitignore",
+        "go",
+        "hcl",
+        "javascript",
+        "json",
+        "lua",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "puppet",
+        "query",
+        "ruby",
+        "rust",
+        "yaml",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+      },                   -- this is available in the list of official parsers
+      ignore_install = {}, -- list of parsers to ignore installing
+      highlight = {
+        enable = true,
+      },
+      incremental_selection = {
+        enable = true,
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["av"] = "@block.outer",
+            ["iv"] = "@block.inner",
+          },
+        },
+      },
+      endwise = {
+        enable = true,
+      },
+    },
+    config = function(_, opts)
+      local configs = require("nvim-treesitter.configs")
+      configs.setup(opts)
+    end,
+  },
+}
