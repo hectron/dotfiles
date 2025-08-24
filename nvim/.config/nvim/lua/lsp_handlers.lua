b57c6285b3a51beaf9a06b4ac23c34ba4f3a579b
Hector Rios <that.hector@gmail.com>
2025-08-23 19:39:52 -0500

    feat(nvim): Document lsp keymaps

--- nvim/.config/nvim/lua/lsp_handlers.lua
+++ nvim/.config/nvim/lua/lsp_handlers.lua
@@ -1,9 +1,24 @@
 local M = {}
 
 function M.on_attach(_, bufnr)
+  --- Wrapper for `vim.keymap.set`.
+  ---
+  --- Sets default opts values to:
+  --- keymaps are for buffer LSP is attached to
+  --- prefixes description with `LSP: `
+  ---
+  --- @param modes string|string[]
+  --- @param lhs string
+  --- @param rhs string|function
+  --- @param opts? vim.keymap.set.Opts
   local function set(modes, lhs, rhs, opts)
     local defaults = { noremap = true, silent = true, buffer = bufnr }
     local local_opts = vim.tbl_deep_extend("force", defaults, opts or {})
+
+    local lsp_desc_prefix = "LSP: "
+    if local_opts.desc ~= nil and local_opts.desc:sub(1, #lsp_desc_prefix) ~= lsp_desc_prefix then
+      local_opts.desc = lsp_desc_prefix .. local_opts.desc
+    end
 
     vim.keymap.set(modes, lhs, rhs, local_opts)
   end
@@ -12,34 +27,34 @@ function M.on_attach(_, bufnr)
 
   -- Navigation
   set("n", "gD", vim.lsp.buf.declaration, { desc = "[g]o to [D]eclaration" })
-  set("n", "gd", vim.lsp.buf.definition)
-  set("n", "<Leader>D", vim.lsp.buf.type_definition)
-  set("n", "<Leader>f", vim.lsp.buf.format)
+  set("n", "gd", vim.lsp.buf.definition, { desc = "[g]o to [d]efinition" })
+  set("n", "<Leader>D", vim.lsp.buf.type_definition, { desc = "type [D]efinition"})
+  set("n", "<Leader>F", vim.lsp.buf.format, { desc = "[F]ormat" })
 
   -- Information
   set("n", "K", function() vim.lsp.buf.hover({ border = "rounded" }) end, { desc = "Hover" })
   set({ "n", "i" }, "<C-k>", function() vim.lsp.buf.signature_help({ border = "rounded" }) end,
     { desc = "Signature help" })
-  set("n", "gh", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end)
-  set("n", "gi", vim.lsp.buf.implementation)
-  set("n", "gr", vim.lsp.buf.references)
-  set("n", "<Leader>ds", vim.lsp.buf.document_symbol)
+  set("n", "gh", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end, { desc = "inlay hint" })
+  set("n", "gi", vim.lsp.buf.implementation, { desc = "[g]o to [i]mplementation" })
+  set("n", "gr", vim.lsp.buf.references, { desc = "[g]et [r]eferences" })
+  set("n", "<Leader>ds", vim.lsp.buf.document_symbol, { desc = "[d]ocument [s]ymbol" })
 
   -- Diagnostics
-  set("n", "[d", function() vim.diagnostic.jump({ count = -1, float = true }) end)
-  set("n", "]d", function() vim.diagnostic.jump({ count = 1, float = true }) end)
-  set("n", "<Leader>le", vim.diagnostic.open_float)
+  set("n", "[d", function() vim.diagnostic.jump({ count = -1, float = true }) end, { desc = "previous [d]iagnostic" })
+  set("n", "]d", function() vim.diagnostic.jump({ count = 1, float = true }) end, { desc = "next [d]iagnostic" })
+  set("n", "<Leader>le", vim.diagnostic.open_float, { desc = "[l]ine [e]xplain diagnostic" })
   -- set("n", "<Leader>q", vim.set_loclist)
 
   -- Refactoring
-  set("n", "<Leader>rn", vim.lsp.buf.rename)
-  set("n", "<Leader>ca", vim.lsp.buf.code_action)
+  set("n", "<Leader>rn", vim.lsp.buf.rename, { desc = "[r]e[n]ame" })
+  set("n", "<Leader>ca", vim.lsp.buf.code_action, { desc = "[c]ode [a]ction" })
 
   -- Workspaces
-  set("n", "<Leader>wa", vim.lsp.buf.add_workspace_folder)
-  set("n", "<Leader>wr", vim.lsp.buf.remove_workspace_folder)
-  set("n", "<Leader>wl", function() vim.print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end)
-  set("n", "<Leader>ws", vim.lsp.buf.workspace_symbol)
+  set("n", "<Leader>wa", vim.lsp.buf.add_workspace_folder, { desc = "[w]orkspace [a]dd folder" })
+  set("n", "<Leader>wr", vim.lsp.buf.remove_workspace_folder, { desc = "[w]orkspace [r]emove folder" })
+  set("n", "<Leader>wl", function() vim.print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, { desc = "[w]orkspace [l]ist folders" })
+  set("n", "<Leader>ws", vim.lsp.buf.workspace_symbol, { desc = "[w]orkspace [s]ymbol" })
 
   -- Set up diagnostics
   vim.diagnostic.config({
