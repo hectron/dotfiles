fac94bdae3027e33a76ed59941fea7049cb1d50b
Hector Rios <that.hector@gmail.com>
2025-08-25 08:22:25 -0500

    feat: Change leader key to spacebar

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,3 +1,7 @@
 vim.keymap.set({ "n", "v", "o" }, "j", "gj")
 vim.keymap.set({ "n", "v", "o" }, "k", "gk")
 vim.keymap.set("i", "jj", "<Esc>")
+
+local empty_string = " "
+vim.g.mapleader = empty_string
+vim.g.localmapleader = empty_string
