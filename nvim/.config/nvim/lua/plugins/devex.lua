0453896934ef58cc19baf559602b4afe11dc1c4c
Hector Rios <that.hector@gmail.com>
2026-03-08 17:57:04 -0500

    feat(nvim): octo.nvim for GH reviews

--- nvim/.config/nvim/lua/plugins/devex.lua
+++ nvim/.config/nvim/lua/plugins/devex.lua
@@ -160,5 +160,51 @@ return {
     keys = {
         { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
+  },
+  {
+    "pwntester/octo.nvim",
+    cmd = "Octo",
+    opts = {
+      -- or "fzf-lua" or "snacks" or "default"
+      picker = "fzf-lua",
+      -- bare Octo command opens picker of commands
+      enable_builtin = true,
+    },
+    keys = {
+      {
+        "<leader>gi",
+        "<CMD>Octo issue list<CR>",
+        desc = "List GitHub Issues",
+      },
+      {
+        "<leader>gp",
+        "<CMD>Octo pr list<CR>",
+        desc = "List GitHub PullRequests",
+      },
+      {
+        "<leader>gd",
+        "<CMD>Octo discussion list<CR>",
+        desc = "List GitHub Discussions",
+      },
+      {
+        "<leader>gn",
+        "<CMD>Octo notification list<CR>",
+        desc = "List GitHub Notifications",
+      },
+      {
+        "<leader>gs",
+        function()
+          require("octo.utils").create_base_search_command { include_current_repo = true }
+        end,
+        desc = "Search GitHub",
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "ibhagwan/fzf-lua",
+      -- OR "nvim-telescope/telescope.nvim",
+      -- OR "folke/snacks.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
   }
 }
